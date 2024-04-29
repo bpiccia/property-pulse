ac16f2dcbc99e22bccf1f292e9ec1ec6bda694b6
Bruno Picciafuoco <11687958+bpiccia@users.noreply.github.com>
2024-04-29 17:38:45 -0300

    feat: added loading

--- app/page.jsx
+++ app/page.jsx
@@ -2,11 +2,14 @@ import Hero from '@/components/Hero'
 import InfoBoxes from '@/components/InfoBoxes'
 import HomeProperties from '@/components/HomeProperties'
 
-const HomePage = () =>
+const HomePage = () => {
+    return (
     <>
         <Hero/>
         <InfoBoxes></InfoBoxes>
         <HomeProperties></HomeProperties>
     </>
+    )
+}
 
 export default HomePage
