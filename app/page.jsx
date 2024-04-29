b04091be4bb5fba40d2e8524ff9cd914b117d7cd
Bruno Picciafuoco <11687958+bpiccia@users.noreply.github.com>
2024-04-29 16:23:04 -0300

    feat: creating not found page

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,12 @@
 import Hero from '@/components/Hero'
 import InfoBoxes from '@/components/InfoBoxes'
+import HomeProperties from '@/components/HomeProperties'
 
 const HomePage = () =>
     <>
         <Hero/>
         <InfoBoxes></InfoBoxes>
+        <HomeProperties></HomeProperties>
     </>
 
 export default HomePage
